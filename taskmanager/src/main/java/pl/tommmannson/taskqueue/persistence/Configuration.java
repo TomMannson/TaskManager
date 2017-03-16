f54c9bc4d428493784d5008205302bade94f27f6
Tomasz Król <tomasz.krol88@gmail.com>
2017-03-16 16:28:56 +0100

    cleanup and bugfix

--- taskmanager/src/main/java/pl/tommmannson/taskqueue/persistence/Configuration.java
+++
@@ -1,8 +0,0 @@
-package pl.tommmannson.taskqueue.persistence;
-
-/**
- * Created by tomasz.krol on 2017-03-15.
- */
-
-public class Configuration {
-}
---
+++ taskqueuesample/src/main/java/pl/tommmannson/taskqueuesample/SampleJob.java
@@ -0,0 +1,8 @@
+package pl.tommmannson.taskqueuesample;
+
+/**
+ * Created by tomasz.krol on 2017-03-16.
+ */
+
+public class SampleJob {
+}
